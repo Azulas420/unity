2c347f1fffbb1ad6a604c6473898658a47294996
Azulas420 <luismedeiros.eprosec@gmail.com>
2023-06-16 17:18:54 +0000

    cenas

--- Assets/Scripts/BulletTurretLV1Script.cs
+++ Assets/Scripts/BulletTurretLV1Script.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class BulletTurretLV1Script : MonoBehaviour
 {
-    public float Range;
-    public Transform Target;
-    public bool playerIsClose = false;
-    Vector2 Direction;
-    public GameObject Gun;
+    public bool playerIsClose;
+    public GameObject Weapon;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -19,38 +17,7 @@ public class BulletTurretLV1Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 targetPos = Target.position;
-
-        Direction = targetPos - (Vector2)transform.position;
         
-        RaycastHit2D rayInfo = Physics2D.Raycast(transform.position,Direction,Range);
-
-        if(rayInfo)
-        {
-            if(rayInfo.collider.gameObject.tag == "Player")
-            {
-                if(playerIsClose == false)
-                {
-                    playerIsClose = true;
-                }          
-            }
-            else 
-            {
-                if(playerIsClose == true)
-                {
-                    playerIsClose = false;
-                    Debug.Log("coda");
-                }          
-            }
-        }
-        if(playerIsClose)
-        {
-            Gun.transform.up = Direction;
-        }
     }
 
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.DrawWireSphere(transform.position,Range);
-    }
 }
