6161b051686e7486e4cb0daa87bac74d298c0e91
Azulas420 <luismedeiros.eprosec@gmail.com>
2023-06-21 12:09:47 +0000

    GM2

--- Assets/Scripts/BulletTurretLV1Script.cs
+++ Assets/Scripts/BulletTurretLV1Script.cs
@@ -6,15 +6,22 @@ public class BulletTurretLV1Script : MonoBehaviour
 {
     public bool playerIsClose;
     public GameObject Weapon;
+    public GameObject Bullet;
+    public float FireRate;
+    float nextTimeToFire = 0;
     public float Range;
+    public float Force;
+    public Transform ShootPoint;
+    public Transform ShootPoint2;
     public Transform? Target;
+
     Vector2 Direction;
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
-         
+        Bullet.transform.up = Direction;
     }
 
     // Update is called once per frame
@@ -24,14 +31,11 @@ public class BulletTurretLV1Script : MonoBehaviour
         if (Target != null)
         {
 
-
             Vector2 targetPos = Target.position;
 
             Direction = targetPos - (Vector2)transform.position;
 
             RaycastHit2D rayInfo = Physics2D.Raycast(transform.position, Direction, Range);
-
-
 
             if (rayInfo)
             {
@@ -42,20 +46,42 @@ public class BulletTurretLV1Script : MonoBehaviour
                         playerIsClose = true;
                     }
                 }
-            }
 
-            else
-            {
-                if (playerIsClose == true)
+                else
                 {
-                    playerIsClose = false;
+                    if (playerIsClose == true)
+                    {
+                        playerIsClose = false;
+                    }
                 }
             }
         }
-        
-        void OnDrawGizmosSelected()
+        if(playerIsClose)
         {
-            Gizmos.DrawWireSphere(transform.position, Range);
+            Weapon.transform.up = Direction;
+            if (Time.time > nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1 / FireRate;
+                shoot();
+            }
         }
+    }
+
+    void shoot()
+    {
+        GameObject ShootIns = Instantiate(Bullet,ShootPoint.position, Quaternion.identity);
+        ShootIns.GetComponent<Rigidbody2D>().AddForce(Direction * Force);
+
+        GameObject ShootIns2 = Instantiate(Bullet, ShootPoint2.position, Quaternion.identity);
+        ShootIns2.GetComponent<Rigidbody2D>().AddForce(Direction * Force);
+
+        if (Target != null)
+        {
+            playerIsClose = false;
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, Range);
     }
 }
