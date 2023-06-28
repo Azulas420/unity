2b33a5fb6e90b7d1c3e6855238e05564bd23ac0d
Azulas420 <luismedeiros.eprosec@gmail.com>
2023-06-28 21:36:15 +0000

    s

--- Assets/Scripts/PlayerDeathScript.cs
+++ Assets/Scripts/PlayerDeathScript.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDeathScript : MonoBehaviour
-{
+{    
+    public bool playerIsDead;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,13 @@ public class PlayerDeathScript : MonoBehaviour
     }  
         void OnCollisionEnter2D (Collision2D col)
         {
-            if (col.gameObject.tag.Equals ("Bullet"))
+            if (col.gameObject.tag.Equals ("Bullet") && !playerIsDead)
             {
+                playerIsDead = true;
                 Destroy (col.gameObject);
                 Destroy (gameObject);
             }
         }
     
 }
+
