15f72afea4404b0bdbe5661c909dec5279670d88
Azulas420 <luismedeiros.eprosec@gmail.com>
2023-07-14 01:03:56 +0000

    nada

--- Assets/Scripts/DeathScreenScript.cs
+++ Assets/Scripts/DeathScreenScript.cs
@@ -7,18 +7,6 @@ using UnityEngine.SceneManagement;
 public class DeathScreenScript : MonoBehaviour
 {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // O código de inicialização pode ser adicionado aqui, se necessário
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // O código de atualização pode ser adicionado aqui, se necessário
-    }
-
     // Reinicia o jogo
     public void Restart()
     {
